a79c5c04ed191a69b2704dde3e4894f6b7370ac2
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-22 18:52:21 -0400

    server can start and show stuff

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
 
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,22 +14,25 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req,res) => {
-    res.render("homepage")
-})
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
-
-
-
-  
-
-
-
+  app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
